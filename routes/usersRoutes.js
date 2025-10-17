0e669404a4c095d26e8c3454e00599eda25d7d23
c0argpat <c0argpat@taniroda-hp-1>
2025-10-17 14:26:18 +0200

    kész

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,35 +1,92 @@
 import express from "express"
-import * as User from "../data/user.js"
+import * as Users from "../data/user.js"
+import bcrypt, { hash } from "bcrypt"
+import jwt from "jsonwebtoken"
+
 const router = express.Router()
 
 router.get("/", (req, res) => {
-    const users = User.getUsers()
-    res.send("Users")
+    const users = Users.getUsers()
+    res.json(users)
 })
 
-router.get("/:id", (req, res) => {
-    const users = User.getUserById()
-    res.send("User")
+
+router.post("/register", (req, res) => {
+    const {name, email, password} = req.body
+    if(!name || !email || !password){
+        return res.status(400).json({message: "Missing required data"});
+    }
+    let user = Users.getUserByEmail(email)
+    if (user){
+        return res.status(400).json({message: "User not found"})
+    }
+    const salt = bcrypt.genSaltSync(12)
+    const hashedPassword = bcrypt.hashSync(password, salt)
+    const saved = Users.saveUser(name, email, hashedPassword)
+    user = Users.getUserById(saved.lastInsertRowid)
+    delete user.password
+    res.status(201).json(user)
 })
 
-router.get("/email", (req, res) => {
-    const users = User.getUserByEmail()
-    res.send("User")
+router.post("/login", (req, res) => {
+     const {email, password} = req.body
+    if(!email || !password){
+        return res.status(400).json({message: "Missing required data"});
+    }
+    let user = Users.getUserByEmail(email)
+    if (user){
+        return res.status(400).json({message: "Invalid credentials"})
+    }
+    if(!bcrypt.compareSync(password, user.password)){
+        return res.status(400).json({message: "Invalid credentials"})
+    }
+    const token = jwt.sign({id: user.id, email: user.email}, "secret_key",{expiresIn: "30s"});
+    res.status(201).json(token)
 })
 
-router.post("/", (req, res) => {
-    const users = User.updateUser()
-    res.send("User")
+router.patch("/:id", auth, (req, res) => {
+    const id = +req.userId
+    const {name, email, password} = req.body
+    let hashedPassword;
+    if(password){
+        const salt = bcrypt.genSaltSync(12)
+        hashedPassword = bcrypt.hashSync(password, salt)
+    }
+    let user = Users.getUserById(id)
+    Users.updateUser(
+        id, 
+        name || user.name, 
+        email || user.email, 
+        hashedPassword || user.password)
+    user = Users.getUserById(saved.lastInsertRowid)
+    delete user.password
+    res.status(200).json(user)
 })
 
-router.put("/:id", (req, res) => {
-    const users = User.saveUser()
-    res.send("User")
+router.get("/me", auth, (req, res) => {
+    const user = Users.getUserById(+req.userId)
+    delete user.password
+    res.json(user)
 })
 
-router.delete("/:id", (req, res) => {
-    const users = User.deleteUser()
-    res.send("User")
-})
+function auth(req, res, next){
+    try{
+        const accessToken = req.headers.authorization
+        if(!accessToken){
+            return res.status(401).json({message: "Unauthorized"})
+        }
+        const token = accessToken.split(' ')[1]
+        const data = jwt.verify(token, "secret_key")
+        const now = Math.floor(Date.now() / 1000)
+        if(data?.exp < now){
+            return res.status(403).json({message: "Token expired"})
+        }
+        req.userId = data.id
+        req.userEmail = data.email
+        next()
+    }catch(err){
+        res.status(500).json({error: err.message})
+    }
+}
 
 export default router
