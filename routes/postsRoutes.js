e383c0941cd014f26dbef54825ef91da3af54936
c0argpat <c0argpat@taniroda-hp-1>
2025-11-25 08:25:54 +0100

    e

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,10 +1,13 @@
-import { Router } from "express";
+import express from "express";
+import * as Posts from "../data/post.js"
+import bcrypt, { hash } from "bcrypt"
+import jwt from "jsonwebtoken"
 
-const router = Router()
+const router = express.Router()
 
 router.get('/', (req, res) =>{
-    const post = Pos
-    res.send("Posts")
+    const posts = Posts.getPosts()
+    res.send(posts)
 })
 
 export default router
